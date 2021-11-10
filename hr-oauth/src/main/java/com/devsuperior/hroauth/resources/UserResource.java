a66016b5488069ec9c2b54fd0b197e6eb84b758d
edumazaro21 <edumazaro21@gmail.com>
2021-11-10 11:53:30 -0300

    Create genereted token

--- hr-oauth/src/main/java/com/devsuperior/hroauth/resources/UserResource.java
+++ hr-oauth/src/main/java/com/devsuperior/hroauth/resources/UserResource.java
@@ -5,6 +5,7 @@ import com.devsuperior.hroauth.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -26,6 +27,9 @@ public class UserResource {
             return ResponseEntity.ok(user);
 
         } catch (IllegalArgumentException ex) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+
+        } catch (UsernameNotFoundException ex) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
     }
